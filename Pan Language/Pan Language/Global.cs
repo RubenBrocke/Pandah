2a5aca3f634647447174dbc46a2d9ecce37b6d36
PrincePanda <ruben@marienhove.nl>
2017-07-07 12:47:33 +0200

    Really added Global.cs

---
+++ Pan Language/Pan Language/Global.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pan_Language
+{
+    static class Global
+    {
+        public static List<Class> Classes = new List<Class>();
+
+        public static Class GetClassById(string idName)
+        {
+            foreach(Class c in Classes)
+            {
+                if (c.instanceIDs.Any(s => s == idName))
+                {
+                    return c;
+                }
+            }
+            return null;
+        }
+
+        public static Class GetClass(string className)
+        {
+            return Classes.Find(c => c.ClassName == className);
+        }
+    }
+}
